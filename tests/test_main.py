367d5c569af4e25c5f010fd5c159fac6961755ed
AndriiTsymbalUKENL1 <s172966@student.uken.krakow.pl>
2026-01-17 21:41:03 +0100

    Create test_main.py
    Twożymy testy dla wdrożenia CI

---
+++ tests/test_main.py
@@ -0,0 +1,45 @@
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine, StaticPool
+from sqlalchemy.orm import sessionmaker
+from app.main import app
+from app.database import Base
+from app.routers.books import get_db
+
+# Używamy bazy w pamienci RAM żeby nie twożyć zbędnuch plików
+SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool,
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+#Podmieniamy baze dannych
+app.dependency_overrides[get_db] = override_get_db
+
+client = TestClient(app)
+
+def setup_module(module):
+    Base.metadata.create_all(bind=engine)
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+
+#Dodajemy książkę
+def test_create_and_read_book():
+    response = client.post(
+        "/add-book",
+        data={"title": "Test", "TestKtos": "Ttttt", "year": 2024},
+        follow_redirects=False
+    )
+    assert response.status_code == 303
